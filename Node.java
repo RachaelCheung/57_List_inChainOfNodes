1d83f666f07e020f667ec75914917384cc9dd6ea
Rachael Cheung <rcheung00@stuy.edu>
2019-03-22 14:01:37 -0400

    working UserOfNode

--- Node.java
+++ Node.java
@@ -13,13 +13,17 @@ public class Node {
       Construct an instance
      */
     public Node( Object cargoReference) {
+		this.cargoReference = cargoReference;
     }
-    // public Node( Object cargoReference, Node referenceToNextNode) {
-        // /* For incremental development with the skeleton's UserOfNode,
-           // postpone writing this constructor until after the accessors.
-           // Then remove this comment, of course.
-         // */
-    // }
+	
+    public Node( Object cargoReference, Node referenceToNextNode) {
+        /* For incremental development with the skeleton's UserOfNode,
+           postpone writing this constructor until after the accessors.
+           Then remove this comment, of course.
+         */
+		this(cargoReference);
+		this.referenceToNextNode = referenceToNextNode;
+    }
 
     /**
       @return a string representation of this instance
@@ -38,10 +42,12 @@ public class Node {
         return result;
     }
 
-    // // classic accessor and mutator
-    // public Node getReferenceToNextNode() {
-    // }
+    // classic accessor and mutator
+    public Node getReferenceToNextNode() {
+		return referenceToNextNode;
+    }
 
-    // public void setReferenceToNextNode( Node referenceToNextNode) {
-    // }
+    public void setReferenceToNextNode( Node referenceToNextNode) {
+		this.referenceToNextNode = referenceToNextNode;
+    }
 }
